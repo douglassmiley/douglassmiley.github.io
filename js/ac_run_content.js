492a9de413f2fd30156f94674719a1fc9356690c
douglassmiley <dsmiley@gmail.com>
2018-08-18 12:59:54 -0400

    update
    update

---
+++ js/ac_run_content.js
@@ -0,0 +1,23 @@
+// JavaScript Document
+AC_FL_RunContent(
+'codebase', 'http://download.macromedia.com/pub/shockwave/cabs/flash/swflash.cab#version=10,0,0,0',
+'width', '400',
+'height', '300',
+'src', 'shelbyscopybutton',
+'quality', 'high',
+'pluginspage', 'http://www.adobe.com/go/getflashplayer',
+'align', 'middle',
+'play', 'true',
+'loop', 'true',
+'scale', 'showall',
+'wmode', 'window',
+'devicefont', 'false',
+'id', 'shelbyscopybutton',
+'bgcolor', '#3a70b8',
+'name', 'shelbyscopybutton',
+'menu', 'true',
+'allowFullScreen', 'false',
+'allowScriptAccess','sameDomain',
+'movie', 'shelbyscopybutton',
+'salign', ''
+);
